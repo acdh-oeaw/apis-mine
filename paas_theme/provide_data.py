3dd722d059cff60dde5dcae6f29c6d7ae63e9d00
Matthias Schlögl <m.schloegl@gmail.com>
2021-04-20 12:20:58 +0000

    changed kommissionen for double dates

--- paas_theme/provide_data.py
+++ paas_theme/provide_data.py
@@ -610,15 +610,21 @@ def enrich_person_context(person_object, context):
             "Mitglied in einer nationalsozialistischen Vereinigung"
         ].append("Registrierungspflicht aufgrund des Verbotsgesetzes vom 1.5.1945")
     if person_object.personinstitution_set.filter(relation_type_id=26).count() > 0:
+        lst_kom = dict()
+        for rel in person_object.personinstitution_set.filter(
+            relation_type_id=26
+        ).order_by("start_date"):
+            if rel.related_institution not in lst_kom.keys():
+                lst_kom[rel.related_institution] = [
+                    get_date_range(rel, classes["time_ranges_ids"])[1:-1]
+                ]
+            else:
+                lst_kom[rel.related_institution].append(
+                    get_date_range(rel, classes["time_ranges_ids"])[1:-1]
+                )
         lst_kom = [
-            (rel.related_institution, get_date_range(rel, classes["time_ranges_ids"]))
-            for rel in person_object.personinstitution_set.filter(
-                relation_type_id=26
-            ).order_by("start_date")
-        ]
-        lst_kom = [
-            f'<a href="/institution/{inst[0].pk}">{inst[0].name}</a> {inst[1]}'
-            for inst in lst_kom
+            f'<a href="/institution/{inst.pk}">{inst.name}</a> ({", ".join(dates)})'
+            for inst, dates in lst_kom.items()
         ]
         context["daten_akademie"]["Funktionen in der Akademie"].append(
             f'Mitglied der folgenden Kommission{"en" if len(lst_kom) > 1 else ""}: {", ".join(lst_kom)}'
