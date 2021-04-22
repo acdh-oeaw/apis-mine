ea533fc973aebf8083366286ae3180afdbab5861
Barbara Krautgartner <barbara.krautgartner@oeaw.ac.at>
2021-04-22 13:39:35 +0200

    kleine style ergänzungen (liste komissionen)

--- paas_theme/provide_data.py
+++ paas_theme/provide_data.py
@@ -638,7 +638,7 @@ def enrich_person_context(person_object, context):
             for inst, dates in lst_kom.items()
         ]
         context["daten_akademie"]["Funktionen in der Akademie"].append(
-            f'Mitglied der folgenden Kommission{"en" if len(lst_kom) > 1 else ""}: <ul>{"".join(lst_kom)}</ul>'
+            f'Mitglied der folgenden Kommission{"en" if len(lst_kom) > 1 else ""}: <ul class="list-unstyled pl-3">{"".join(lst_kom)}</ul>'
         )
     if (
         "Mitglied in einer nationalsozialistischen Vereinigung"
