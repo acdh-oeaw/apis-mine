6b5f76a4dbd6fdd60be448785fe344dd25c4cc76
Matthias Schlögl <m.schloegl@gmail.com>
2021-05-05 15:20:39 +0000

    wrong path

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 python manage.py migrate --settings=apis.settings.dev
-ls /var/paas_solr
-python manage.py build_solr_schema --configure-directory /var/solr/paas_solr/conf --reload-core default
+ls /var/solr_new/paas_solr
+python manage.py build_solr_schema --configure-directory /var/solr_new/paas_solr/conf --reload-core default
 gunicorn apis.wsgi
