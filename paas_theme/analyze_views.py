05c6a915f04b31d96631b40d612476363d2bb85b
csae8092 <peter.andorfer@oeaw.ac.at>
2021-08-31 19:34:22 +0000

    group nazi-kommissionen by 'grouped=mitglied|kommission' #11

--- paas_theme/analyze_views.py
+++ paas_theme/analyze_views.py
@@ -27,8 +27,48 @@ def get_nazi_kommissionen(request):
         related_institution__in=KOMMISSIONEN
     ).values_list(*props)
     orig_df = pd.DataFrame(list(rels), columns=props)
-    return JsonResponse(orig_df.to_dict('records'), safe=False)
-
+    grouped = request.GET.get('grouped')
+    if grouped == 'kommission':
+        data = []
+        for gr, df in orig_df.groupby('related_institution__id'):
+            item = {}
+            item['id'] = gr
+            first_row = df.iloc[0]
+            item['name'] = first_row['related_institution__name']
+            members = {}
+            for i, row in df.iterrows():
+                member_name = f"{row['related_person__name']}, {row['related_person__first_name']}"
+                members[row['related_person__id']] = {
+                    'name': member_name,
+                    'start_date': f"{row['start_date']}",
+                    'end_date': f"{row['end_date']}",
+                    'rel_type': row['relation_type__name']
+                }
+            item['members'] = members
+            item['member_amount'] = len(members)
+            data.append(item)
+    elif grouped == "mitglied":
+        data = []
+        for gr, df in orig_df.groupby('related_person__id'):
+            item = {}
+            item['id'] = gr
+            first_row = df.iloc[0]
+            item['name'] = f"{first_row['related_person__name']}, {first_row['related_person__first_name']}"
+            members = {}
+            for i, row in df.iterrows():
+                member_name = f"{row['related_institution__name']}"
+                members[row['related_institution__id']] = {
+                    'name': member_name,
+                    'start_date': f"{row['start_date']}",
+                    'end_date': f"{row['end_date']}",
+                    'rel_type': row['relation_type__name']
+                }
+            item['members'] = members
+            item['amount'] = len(members)
+            data.append(item)
+    else:
+        data = orig_df.to_dict('records')
+    return JsonResponse(data, safe=False)
 
 
 def ns_view(request):
