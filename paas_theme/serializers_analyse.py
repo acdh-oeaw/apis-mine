ec4d4a761f989287637e202337bf37fa83c35d35
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2021-09-02 19:20:21 +0000

    performance optimizations

--- paas_theme/serializers_analyse.py
+++ paas_theme/serializers_analyse.py
@@ -17,17 +17,28 @@ class KommissionZeitstrahl(serializers.Serializer):
             end = datetime.date.today()
         if end is None or obj.start_date is None:
             return [] 
-        for year in range(int(obj.start_date.strftime("%Y")), int(end.strftime("%Y"))):
-            res[year] = members.filter(start_date__lte=f"{str(year)}-12-31", end_date__gte=f"{str(year)}-01-01").count()
+        # for year in range(int(obj.start_date.strftime("%Y")), int(end.strftime("%Y"))):
+        #     res[year] = members.filter(start_date__lte=f"{str(year)}-12-31", end_date__gte=f"{str(year)}-01-01").count()
+        for mem in members:
+            for year in range(int(obj.start_date.strftime("%Y")), int(end.strftime("%Y"))):
+                if year not in res.keys():
+                    res[year] = 0
+                if mem.end_date is None:
+                    mem_end = datetime.date.today()
+                else:
+                    mem_end = mem.end_date
+                if year in range(int(mem.start_date.strftime("%Y")), int(mem_end.strftime("%Y"))):
+                    res[year] += 1
         return res
 
 
 class KommissionenZeitstrahlNazis(KommissionZeitstrahl):
-    mitglieder_nsdap = serializers.SerializerMethodField(method_name="count_nazi_member")
+    mitglieder_ns_organisation = serializers.SerializerMethodField(method_name="count_nazi_member")
 
     def count_nazi_member(self, obj):
         res = dict()
         members = self._members.filter(related_person__in=NATIONALSOZIALISTEN)
+        members_dict = dict()
         end = obj.end_date
         if end is None:
             end = datetime.date.today()
@@ -35,10 +46,21 @@ class KommissionenZeitstrahlNazis(KommissionZeitstrahl):
             return [] 
         for year in range(int(obj.start_date.strftime("%Y")), int(end.strftime("%Y"))):
             count = 0
-            mem = members.filter(start_date__lte=f"{str(year)}-12-31", end_date__gte=f"{str(year)}-01-01")
-            for m in mem:
-                if m.related_person.personinstitution_set.filter(relation_type__in=[3452, 3451], start_date__lte=f"{str(year)}-12-31").count() > 0:
-                    count += 1
+            #mem = members.filter(start_date__lte=f"{str(year)}-12-31", end_date__gte=f"{str(year)}-01-01")
+            for m in members:
+                if m.end_date is None:
+                    mem_end = datetime.date.today()
+                else:
+                    mem_end = m.end_date
+                if year not in range(int(m.start_date.strftime("%Y")), int(mem_end.strftime("%Y"))):
+                    continue
+                if m.related_person_id in members_dict.keys():
+                    if members_dict[m.related_person_id] <= year:
+                        count += 1
+                else:
+                    r1 = m.related_person.personinstitution_set.filter(relation_type__in=[3452, 3451], start_date__lte=f"{str(year)}-12-31").order_by("start_date")
+                    if r1.count() > 0:
+                        count += 1
+
             res[year] = count
         return res
-
