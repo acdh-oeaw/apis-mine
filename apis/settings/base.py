d143342930a26d651bf798d30e20625a3a7c90e2
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2021-09-20 13:02:54 +0000

    removed X-origin in favour of csp

--- apis/settings/base.py
+++ apis/settings/base.py
@@ -139,7 +139,6 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "csp.middleware.CSPMiddleware",
     "reversion.middleware.RevisionMiddleware",
     "crum.CurrentRequestUserMiddleware",
