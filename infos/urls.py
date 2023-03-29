49ac89363e622e2ce1afb21724919d8e5b6c69e9
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2023-03-29 10:16:25 +0200

    moves urls to new patterns

--- infos/urls.py
+++ infos/urls.py
@@ -1,89 +1,76 @@
-from django.conf.urls import url
+from django.urls import path, re_path
 from . import views
 from . import special_views
 
-app_name = 'infos'
+app_name = "infos"
 urlpatterns = [
-    url(
-        r'^project-team/$',
-        special_views.TeamView.as_view(),
-        name='project-team'
-    ),
-    url(
-        r'^about-the-project/$',
+    path(r"project-team/", special_views.TeamView.as_view(), name="project-team"),
+    path(
+        r"about-the-project/",
         special_views.SpecialAboutView.as_view(),
-        name='about-the-project'
+        name="about-the-project",
     ),
-    url(
-        r'^about/$',
-        views.AboutTheProjectListView.as_view(),
-        name='about_browse'
-    ),
-    url(
-        r'^about/detail/(?P<pk>[0-9]+)$',
+    path(r"about/", views.AboutTheProjectListView.as_view(), name="about_browse"),
+    re_path(
+        r"^about/detail/(?P<pk>[0-9]+)$",
         views.AboutTheProjectDetailView.as_view(),
-        name='about_detail'
+        name="about_detail",
     ),
-    url(
-        r'^about/create/$',
-        views.AboutTheProjectCreate.as_view(),
-        name='about_create'
-    ),
-    url(
-        r'^about/edit/(?P<pk>[0-9]+)$',
+    path(r"about/create/", views.AboutTheProjectCreate.as_view(), name="about_create"),
+    re_path(
+        r"^about/edit/(?P<pk>[0-9]+)$",
         views.AboutTheProjectUpdate.as_view(),
-        name='about_edit'
+        name="about_edit",
     ),
-    url(
-        r'^about/delete/(?P<pk>[0-9]+)$',
+    re_path(
+        r"^about/delete/(?P<pk>[0-9]+)$",
         views.AboutTheProjectDelete.as_view(),
-        name='about_delete'),
-    url(
-        r'^teammember/$',
-        views.TeamMemberListView.as_view(),
-        name='teammember_browse'
+        name="about_delete",
     ),
-    url(
-        r'^teammember/detail/(?P<pk>[0-9]+)$',
+    path(r"teammember/", views.TeamMemberListView.as_view(), name="teammember_browse"),
+    re_path(
+        r"^teammember/detail/(?P<pk>[0-9]+)$",
         views.TeamMemberDetailView.as_view(),
-        name='teammember_detail'
+        name="teammember_detail",
     ),
-    url(
-        r'^teammember/create/$',
+    path(
+        r"teammember/create/",
         views.TeamMemberCreate.as_view(),
-        name='teammember_create'
+        name="teammember_create",
     ),
-    url(
-        r'^teammember/edit/(?P<pk>[0-9]+)$',
+    re_path(
+        r"^teammember/edit/(?P<pk>[0-9]+)$",
         views.TeamMemberUpdate.as_view(),
-        name='teammember_edit'
+        name="teammember_edit",
     ),
-    url(
-        r'^teammember/delete/(?P<pk>[0-9]+)$',
+    re_path(
+        r"^teammember/delete/(?P<pk>[0-9]+)$",
         views.TeamMemberDelete.as_view(),
-        name='teammember_delete'),
-    url(
-        r'^projectinst/$',
-        views.ProjectInstListView.as_view(),
-        name='projectinst_browse'
+        name="teammember_delete",
     ),
-    url(
-        r'^projectinst/detail/(?P<pk>[0-9]+)$',
+    path(
+        r"projectinst/", views.ProjectInstListView.as_view(), name="projectinst_browse"
+    ),
+    re_path(
+        r"^projectinst/detail/(?P<pk>[0-9]+)$",
         views.ProjectInstDetailView.as_view(),
-        name='projectinst_detail'
+        name="projectinst_detail",
     ),
-    url(
-        r'^projectinst/create/$',
+    path(
+        r"projectinst/create/",
         views.ProjectInstCreate.as_view(),
-        name='projectinst_create'
+        name="projectinst_create",
     ),
-    url(
-        r'^projectinst/edit/(?P<pk>[0-9]+)$',
+    re_path(
+        r"^projectinst/edit/(?P<pk>[0-9]+)$",
         views.ProjectInstUpdate.as_view(),
-        name='projectinst_edit'
+        name="projectinst_edit",
     ),
-    url(
-        r'^projectinst/delete/(?P<pk>[0-9]+)$',
+    re_path(
+        r"^projectinst/delete/(?P<pk>[0-9]+)$",
         views.ProjectInstDelete.as_view(),
-        name='projectinst_delete'),
+        name="projectinst_delete",
+    ),
 ]
+
+
--- paas_theme/urls.py
+++ paas_theme/urls.py
@@ -1,5 +1,4 @@
-from django.conf.urls import url
-
+from django.urls import path
 from . import dal_views
 from . import views
 from . import api_views
@@ -11,112 +10,112 @@ app_name = "paas_theme"
 
 
 urlpatterns = [
-    url(r"^$", views.IndexView.as_view(), name="start"),
-    url(r"^imprint/$", views.ImprintView.as_view(), name="imprint"),
-    url(r"^about/$", views.AboutView.as_view(), name="about"),
-    url(r"^contact/$", views.ContactView.as_view(), name="contact"),
-    url(r"^expert-search/$", views.PersonListView.as_view(), name="expert-search"),
-    url(r"search/$", views.SearchView.as_view(), name="search"),
-    url(r"^institutionen/$", views.IndexInstitutionsView.as_view(), name="institutions"),
-    url(r"^institutionen/search/?$", views.SearchViewInstitutions.as_view(), name="institutionssearch"),
-    url(r"^network/?$", views.network_viz),
-    url(r"^stories/ns-zeit/$", story_views.NationalSozialismusStory.as_view(), name="ns_zeit"),
-    url(r"^stories/test/$", views.TestView.as_view(), name="test_view"),
-    url(r"^stories/$", views.StoriesIndexPage.as_view(), name="stories_index"),
-    url(r"^api/network/$", api_views.NetVizTheme.as_view()),
-    url(r"^api/egonetwork/$", api_views.EgoNetwork.as_view()),
-    url(
-        r"^paas/autocompletes/institution/$",
+    path("", views.IndexView.as_view(), name="start"),
+    path(r"imprint/", views.ImprintView.as_view(), name="imprint"),
+    path(r"about/", views.AboutView.as_view(), name="about"),
+    path(r"contact/", views.ContactView.as_view(), name="contact"),
+    path(r"expert-search/", views.PersonListView.as_view(), name="expert-search"),
+    path(r"search/", views.SearchView.as_view(), name="search"),
+    path(r"institutionen/", views.IndexInstitutionsView.as_view(), name="institutions"),
+    path(r"institutionen/search/?", views.SearchViewInstitutions.as_view(), name="institutionssearch"),
+    path(r"network/?", views.network_viz),
+    path(r"stories/ns-zeit/", story_views.NationalSozialismusStory.as_view(), name="ns_zeit"),
+    path(r"stories/test/", views.TestView.as_view(), name="test_view"),
+    path(r"stories/", views.StoriesIndexPage.as_view(), name="stories_index"),
+    path(r"api/network/", api_views.NetVizTheme.as_view()),
+    path(r"api/egonetwork/", api_views.EgoNetwork.as_view()),
+    path(
+        r"paas/autocompletes/institution/",
         autocompletes.PaasInstitutionAutocomplete.as_view(),
         name="paas_institution_autocomplete",
     ),
-    url(
-        r"^paas/autocompletes/universitaet/$",
+    path(
+        r"paas/autocompletes/universitaet/",
         autocompletes.PaasInstitutionUniAutocomplete.as_view(),
         name="paas_institution_uni_autocomplete",
     ),
-    url(
-        r"^paas/autocompletes/universitaet_habil/$",
+    path(
+        r"paas/autocompletes/universitaet_habil/",
         autocompletes.PaasInstitutionUniHabilitationAutocomplete.as_view(),
         name="paas_institution_uni_habil_autocomplete",
     ),
-    url(
-        r"^paas/autocompletes/habilitation_fach/$",
+    path(
+        r"paas/autocompletes/habilitation_fach/",
         autocompletes.PaasHabilitationFachAutocomplete.as_view(),
         name="paas_institution_habil_fach_autocomplete",
     ),
-    url(
-        r"^paas/autocompletes/beruf/$",
+    path(
+        r"paas/autocompletes/beruf/",
         autocompletes.PaasProfessionAutocomplete.as_view(),
         name="paas_person_beruf_autocomplete",
     ),
-    url(
-        r"^paas/autocompletes/schule/$",
+    path(
+        r"paas/autocompletes/schule/",
         autocompletes.PaasInstitutionSchuleAutocomplete.as_view(),
         name="paas_institution_schule_autocomplete",
     ),
-    url(
-        r"^paas/autocompletes/wiss_austausch/$",
+    path(
+        r"paas/autocompletes/wiss_austausch/",
         autocompletes.PaasPlaceWAustauschAutocomplete.as_view(),
         name="paas_place_wiss_austausch_autocomplete",
     ),
-    url(
-        r"^paas/autocompletes/place_of_birth/$",
+    path(
+        r"paas/autocompletes/place_of_birth/",
         autocompletes.PaasPlaceBirthAutocomplete.as_view(),
         name="paas_place_of_birth_autocomplete",
     ),
-    url(
-        r"^paas/autocompletes/place_of_death/$",
+    path(
+        r"paas/autocompletes/place_of_death/",
         autocompletes.PaasPlaceDeathAutocomplete.as_view(),
         name="paas_place_of_death_autocomplete",
     ),
-    url(
-        r"^paas/autocompletes/position/$",
+    path(
+        r"paas/autocompletes/position/",
         autocompletes.PaasPersonInstitutionPositionAutocomplete.as_view(),
         name="paas_position_autocomplete",
     ),
-    url(
-        r"^person/(?P<pk>[0-9]+)$",
+    path(
+        r"person/<int:pk>",
         views.PersonDetailView.as_view(),
         name="person-detail",
     ),
-    url(
-        r"^institution/(?P<pk>[0-9]+)$",
+    path(
+            r"institution/<int:pk>",
         views.InstitutionDetailView.as_view(),
         name="institution-detail",
     ),
-    url(
-        r"^ac/obel-person/$",
+    path(
+        r"ac/obel-person/",
         dal_views.OeblPersons.as_view(),
         name="obel-person-autocomplete",
     ),
-    url(
-        r"^ac/obel-professions/$",
+    path(
+        r"ac/obel-professions/",
         dal_views.ProfessionAC.as_view(),
         name="obel-professions-autocomplete",
     ),
-    url(
-        r"^analyze/nationalsozialismus/$",
+    path(
+        r"analyze/nationalsozialismus/",
         analyze_views.ns_view,
         name="analyze_nationalsozialismus",
     ),
-    url(
-        r"^analyze/api/kommissionen$",
+    path(
+        r"analyze/api/kommissionen",
         api_views.GetKommissionen.as_view(),
         name="get_kommissionen",
     ),
-    url(
-        r"^analyze/nationalsozialismus-kommissionen/$",
+    path(
+        r"analyze/nationalsozialismus-kommissionen/",
         analyze_views.get_nazi_kommissionen,
         name="analyze_nationalsozialismus_kommissionen",
     ),
-    url(
-        r"^analyze/nationalsozialismus-vorschlag/$",
+    path(
+        r"analyze/nationalsozialismus-vorschlag/",
         analyze_views.proposed_by_nazi,
         name="analyze_nationalsozialismus_vorschlag",
     ),
-    url(
-        r"^analyze/mitglieder/$",
+    path(
+        r"analyze/mitglieder/",
         analyze_views.mitglieder,
         name="mitglieder",
     )
